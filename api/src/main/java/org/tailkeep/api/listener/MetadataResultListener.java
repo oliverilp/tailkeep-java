76752dd4cf970b5102b3f34a3de5775fa3909bfe
Oliver Ilp <oliver@oliverilp.ee>
2024-11-16 15:00:45 +0200

    Save data to db for new Kafka messages

--- api/src/main/java/org/tailkeep/api/listener/MetadataResultListener.java
+++ api/src/main/java/org/tailkeep/api/listener/MetadataResultListener.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Component;
 import org.tailkeep.api.config.KafkaTopicNames;
 import org.tailkeep.api.dto.DownloadRequestMessage;
 import org.tailkeep.api.dto.MetadataResultMessage;
+import org.tailkeep.api.model.Channel;
+import org.tailkeep.api.model.Job;
+import org.tailkeep.api.model.Video;
+import org.tailkeep.api.service.ChannelService;
+import org.tailkeep.api.service.JobService;
+import org.tailkeep.api.service.VideoService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,18 +19,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MetadataResultListener {
     private final KafkaTemplate<String, DownloadRequestMessage> downloadKafkaTemplate;
+    private final ChannelService channelService;
+    private final VideoService videoService;
+    private final JobService jobService;
 
-    public MetadataResultListener(KafkaTemplate<String, DownloadRequestMessage> downloadRequestKafkaTemplate) {
+    public MetadataResultListener(
+            KafkaTemplate<String, DownloadRequestMessage> downloadRequestKafkaTemplate,
+            ChannelService channelService,
+            VideoService videoService,
+            JobService jobService) {
         this.downloadKafkaTemplate = downloadRequestKafkaTemplate;
+        this.channelService = channelService;
+        this.videoService = videoService;
+        this.jobService = jobService;
     }
 
     @KafkaListener(topics = KafkaTopicNames.METADATA_RESULTS, groupId = "metadata-results-consumer", containerFactory = "metadataFactory")
     public void listen(MetadataResultMessage message) {
         log.info("Received metadata result: {}", message);
 
+        Channel channel = channelService.getOrCreateChannel(message.metadata());
+        Video video = videoService.createOrUpdateVideo(message.metadata(), channel);
+        
+        Job job = jobService.updateJobMetadata(message.jobId(), video);
+
         DownloadRequestMessage downloadRequest = new DownloadRequestMessage(
-                message.jobId(),
-                message.metadata().youtubeId(),
+                job.getId(),
+                video.getYoutubeId(),
                 message.metadata().url(),
                 message.metadata().filename());
         downloadKafkaTemplate.send(KafkaTopicNames.DOWNLOAD_QUEUE, downloadRequest);
