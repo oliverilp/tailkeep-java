2ac8a8186833fa296cbbd80d3ce25c8278f8e7b4
Oliver Ilp <oliver@oliverilp.ee>
2024-11-20 17:05:26 +0200

    Remove unneeded code

--- api/src/main/java/org/tailkeep/api/config/SecurityConfiguration.java
+++ api/src/main/java/org/tailkeep/api/config/SecurityConfiguration.java
@@ -3,16 +3,13 @@ package org.tailkeep.api.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import static org.tailkeep.api.model.user.Permission.ADMIN_CREATE;
 import static org.tailkeep.api.model.user.Permission.ADMIN_DELETE;
@@ -42,7 +39,6 @@ public class SecurityConfiguration {
             "/swagger-ui.html"};
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -61,12 +57,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-                // .logout(logout ->
-                //         logout.logoutUrl("/api/v1/auth/logout")
-                //                 .addLogoutHandler(logoutHandler)
-                //                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
-                // );
-
+        
         return http.build();
     }
 }
