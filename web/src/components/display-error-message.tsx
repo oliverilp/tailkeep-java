5d72cd81716e14ae5d8f5f4b8539ae5ef3285af8
Oliver Ilp <oliver@oliverilp.ee>
2024-11-25 10:46:03 +0200

    Fix error messages

--- web/src/components/display-error-message.tsx
+++ web/src/components/display-error-message.tsx
@@ -1,11 +1,13 @@
 import { CircleAlert } from 'lucide-react';
 
 export default function ErrorMessage({ text }: { text: string }) {
+  const displayText = text?.trim() ? text : "Something went wrong";
+  
   return (
     <div className="self-start">
       <div className="flex items-center gap-1 text-destructive">
         <CircleAlert className="h-4 min-h-4 w-4 min-w-4" />
-        <p>{text}</p>
+        <p>{displayText}</p>
       </div>
     </div>
   );
