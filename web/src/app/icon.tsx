f52cf0145887943a22a753f8ea74ea19f6a6a6be
Oliver Ilp <oliver@oliverilp.ee>
2024-11-27 23:03:09 +0200

    Fix favicon

--- web/src/app/icon.tsx
+++ web/src/app/icon.tsx
@@ -1,5 +1,12 @@
 import { ImageResponse } from 'next/og';
 
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32
+};
+export const contentType = 'image/png';
+
 function Icon() {
   return new ImageResponse(
     (
@@ -34,8 +41,7 @@ function Icon() {
       </div>
     ),
     {
-      width: 32,
-      height: 32
+      ...size
     }
   );
 }
