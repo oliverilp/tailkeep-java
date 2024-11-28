53bf1b697d67baa1398665e2f364bb038a67a091
Oliver Ilp <oliver@oliverilp.ee>
2024-11-28 19:47:48 +0200

    Add demo user to db

--- api/src/main/java/org/tailkeep/api/service/DatabaseSeederService.java
+++ api/src/main/java/org/tailkeep/api/service/DatabaseSeederService.java
@@ -9,6 +9,7 @@ import org.tailkeep.api.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 @Service
@@ -17,24 +18,43 @@ public class DatabaseSeederService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${DEMO_MODE:false}")
+    private String isDemo;
+
     private static final String ADMIN_NICKNAME = "Admin";
     private static final String ADMIN_USERNAME = "admin";
     private static final String ADMIN_PASSWORD = "Admin1Admin1";
     
+    private static final String DEMO_NICKNAME = "Demo";
+    private static final String DEMO_USERNAME = "demo";
+    private static final String DEMO_PASSWORD = "Demo1Demo1";
+    
     @PostConstruct
     public void seed() {
-        // Create admin user if database is empty
+        // Create initial user if database is empty
         if (userRepository.count() != 0) {
             return;
         }
-        
+
+        if ("true".equalsIgnoreCase(isDemo)) {
+            userRepository.save(User.builder()
+                .nickname(DEMO_NICKNAME)
+                .username(DEMO_USERNAME)
+                .password(passwordEncoder.encode(DEMO_PASSWORD))
+                .role(Role.USER)
+                .build());
+            
+            log.info("Seeded demo user");
+            return;
+        }
+
         userRepository.save(User.builder()
             .nickname(ADMIN_NICKNAME)
-            .username(ADMIN_USERNAME)
-            .password(passwordEncoder.encode(ADMIN_PASSWORD))
-            .role(Role.ADMIN)
-            .build());
-        
+                .username(ADMIN_USERNAME)
+                .password(passwordEncoder.encode(ADMIN_PASSWORD))
+                .role(Role.ADMIN)
+                .build());
+            
         log.info("Seeded admin user");
     }
 }
