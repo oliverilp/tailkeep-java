fa9b06667c62a46305331ae6b7ac75356dddeaaf
Oliver Ilp <oliver@oliverilp.ee>
2024-12-13 20:20:30 +0200

    Fix GitHub CI error

--- api/src/test/java/org/tailkeep/api/TailkeepApplicationTests.java
+++ api/src/test/java/org/tailkeep/api/TailkeepApplicationTests.java
@@ -2,8 +2,13 @@ package org.tailkeep.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.tailkeep.api.config.TestContainersConfig;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestContainersConfig.class)
 class TailkeepApplicationTests {
 
 	@Test
--- api/src/test/java/org/tailkeep/api/config/TestContainersConfig.java
+++ api/src/test/java/org/tailkeep/api/config/TestContainersConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration
+@TestConfiguration(proxyBeanMethods = false)
 public class TestContainersConfig {
 
     @Bean
