b7a45c0ab6fd8a1bfdaea094d115ee0be81a8b91
Oliver Ilp <oliver@oliverilp.ee>
2024-12-24 22:02:00 +0200

    Add download service test

--- worker/src/test/java/org/tailkeep/worker/download/DownloadServiceTest.java
+++ worker/src/test/java/org/tailkeep/worker/download/DownloadServiceTest.java
@@ -9,12 +9,14 @@ import org.tailkeep.worker.command.CommandExecutor;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.argThat;
 
 @ExtendWith(MockitoExtension.class)
 class DownloadServiceTest {
@@ -29,24 +31,34 @@ class DownloadServiceTest {
         downloadService = new DownloadService(commandExecutor);
     }
 
-//    @Test
-//    void processDownload_ShouldExecuteCommandAndReportProgress() {
-//        // Given
-//        String jobId = "job123";
-//        String videoId = "video123";
-//        String url = "https://youtube.com/watch?v=dQw4w9WgXcQ";
-//        String filename = "video.mp4";
-//
-//        when(commandExecutor.execute(anyList(), any()))
-//            .thenReturn(CompletableFuture.completedFuture(null));
-//
-//        @SuppressWarnings("unchecked")
-//        Consumer<DownloadProgressMessage> mockProgressCallback = mock(Consumer.class);
-//
-//        // When
-//        downloadService.processDownload(jobId, videoId, url, filename, mockProgressCallback);
-//
-//        // Then
-//        verify(commandExecutor).execute(anyList(), any());
-//    }
+    @Test
+    void processDownload_ShouldExecuteCommandAndReportProgress() {
+        // Given
+        String jobId = "job123";
+        String videoId = "video123";
+        String url = "https://youtube.com/watch?v=dQw4w9WgXcQ";
+        String filename = "video.mp4";
+
+        List<String> expectedArgs = Collections.singletonList(url);
+        
+        when(commandExecutor.execute(argThat(args -> 
+            args.size() == expectedArgs.size() && 
+            args.containsAll(expectedArgs) && 
+            expectedArgs.containsAll(args)
+        ), any()))
+            .thenReturn(CompletableFuture.completedFuture(null));
+
+        @SuppressWarnings("unchecked")
+        Consumer<DownloadProgressMessage> mockProgressCallback = mock(Consumer.class);
+
+        // When
+        downloadService.processDownload(jobId, videoId, url, filename, mockProgressCallback);
+
+        // Then
+        verify(commandExecutor).execute(argThat(args -> 
+            args.size() == expectedArgs.size() && 
+            args.containsAll(expectedArgs) && 
+            expectedArgs.containsAll(args)
+        ), any());
+    }
 }
