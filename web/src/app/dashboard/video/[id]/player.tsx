e97e5a05905aab36bf26e0ed8c2880c885eafb20
Oliver Ilp <oliver@oliverilp.ee>
2024-12-28 15:41:19 +0200

    Fix player CSS bug

--- web/src/app/dashboard/video/[id]/player.tsx
+++ web/src/app/dashboard/video/[id]/player.tsx
@@ -31,16 +31,17 @@ function Player({ video }: PlayerProps) {
   const videoUrl = `${mediaUrl}/media/${video.id}?token=${token}`;
 
   return (
-    <div className="media-fullscreen:rounded-none aspect-video w-full overflow-hidden rounded-xl bg-black font-sans text-white">
+    <div className="aspect-video w-full overflow-hidden rounded-xl border bg-background font-sans text-white media-fullscreen:rounded-none">
       <MediaPlayer
+        className="vds-video-layout"
         // title={video.title}
         src={{ src: videoUrl, type: 'video/mp4' }}
       >
-        <MediaProvider>
+        <MediaProvider className="bg-black media-fullscreen:bg-black">
           <Poster asChild>
             <Image
               alt="Video thumbnail"
-              className="media-fullscreen:rounded-none absolute inset-0 block h-full w-full rounded-xl object-cover opacity-0 transition-opacity data-[visible]:opacity-100"
+              className="absolute inset-0 block h-full w-full rounded-xl object-cover opacity-0 transition-opacity data-[visible]:opacity-100 media-fullscreen:rounded-none"
               fill
               priority
               src={video.thumbnailUrl}
